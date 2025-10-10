e61a6071a03508c933482880b121f0a9b4471aec
loictonneau75 <loictonneau75@gmail.com>
2025-10-10 09:57:34 +0200

    creation de classe reutilisable

--- app/app.php
+++ app/app.php
@@ -2,3 +2,21 @@
 require_once dirname(__DIR__) . "/db/connexion.php";
 require_once dirname(__DIR__) . "/db/utils.php";
 include __DIR__ . "/navbar/navbar.php";
+?>
+<div class="row">
+    <div class="block input-group">
+    <h2>Rejoindre un salon</h2>
+    <input type="text" id="code" placeholder="">
+    <label for="code">Code</label>
+    </div>
+    <div class="block">
+    <h2>Creer un salon</h2>
+    </div>
+</div>
+<div class="row">
+
+</div>
+<div class="row">
+
+</div>
+<div class="test"></div>
