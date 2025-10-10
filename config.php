6c365aa14cdb91d4b3a9de1a7b0fc482781e25c1
loictonneau75 <loictonneau75@gmail.com>
2025-10-10 08:55:44 +0200

    ajout de l'extraction de donné pour le base url

--- config.php
+++ config.php
@@ -1,3 +1,5 @@
 <?php
 define("TITLE", "Draw Your Fate");
-define("BASE_URL", "http://localhost/treachery/");
+$scheme = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http';
+$host = $_SERVER['HTTP_HOST'];
+define("BASE_URL", "$scheme://$host/treachery/");
