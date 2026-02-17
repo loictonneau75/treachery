870ad706148c4116459eac27a8123c8925c68647
loictonneau75 <loictonneau75@gmail.com>
2026-02-17 21:12:32 +0100

    retrait de la keyboardnav

--- app/partial/addCard/customSelect.js
+++ app/partial/addCard/customSelect.js
@@ -34,47 +34,6 @@ function setupOptionSelection(fakeSelect, optionsWrapper, currentValue, input, o
     })
 }
 
-function setupKeyboardNavigation(fakeSelect, optionsWrapper, currentValue, input, optionsList){
-    let focusedIndex = 0
-    fakeSelect.tabIndex = 0 
-    fakeSelect.addEventListener("keydown", e => {
-        if (e.key === "Enter" || e.key === " ") {
-            e.preventDefault()
-            const isOpen = optionsWrapper.style.display === "block"
-            optionsWrapper.style.display = isOpen ? "none" : "block"
-            fakeSelect.classList.toggle("active", !isOpen)
-        } else if (e.key === "ArrowDown") {
-            e.preventDefault()
-            focusedIndex = 0
-            optionsList[focusedIndex].focus()
-        }
-    })
-    optionsList.forEach((option, index) => {
-        option.tabIndex = 0
-        option.addEventListener("keydown", e => {
-            if (e.key === "ArrowDown") {
-                e.preventDefault()
-                focusedIndex = (index + 1) % optionsList.length
-                optionsList[focusedIndex].focus()
-            } else if (e.key === "ArrowUp") {
-                e.preventDefault()
-                focusedIndex = (index - 1 + optionsList.length) % optionsList.length
-                optionsList[focusedIndex].focus()
-            } else if (e.key === "Enter" || e.key === " ") {
-                e.preventDefault()
-                changeSelectedOption(optionsList, option, currentValue, input, optionsWrapper)
-                fakeSelect.classList.remove("active")
-                fakeSelect.focus()
-            } else if (e.key === "Escape") {
-                optionsWrapper.style.display = "none"
-                fakeSelect.classList.remove("active")
-                fakeSelect.focus()
-            }
-        })
-    })
-}
-
-
 function getSelectElements(customSelect) {
     const fakeSelect = customSelect.querySelector(":scope > div")
     const currentValue = fakeSelect.querySelector(":scope div")
@@ -94,7 +53,6 @@ const customSelects = Array.from(document.querySelectorAll(".custom-select")).ma
     initializeDefaultSelection(elements.optionsWrapper, elements.currentValue, elements.input)
     setupToggleBehavior(elements.fakeSelect, elements.optionsWrapper)
     setupOptionSelection(elements.fakeSelect, elements.optionsWrapper, elements.currentValue, elements.input, elements.optionsList)
-    setupKeyboardNavigation(elements.fakeSelect, elements.optionsWrapper, elements.currentValue, elements.input, elements.optionsList)
     return { customSelect, ...elements }
 })
 
