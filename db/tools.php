73d8dc9c9c8e6fbf30accb65d8a98d492ee4e464
loictonneau75 <loictonneau75@gmail.com>
2026-02-20 19:17:09 +0100

    ajout de securité

--- db/tools.php
+++ db/tools.php
@@ -72,16 +72,19 @@ class DbTools{
     }
 
     public static function getAllFrom(PDO $pdo, string $table): array{
+        $allowedTables = ['roles', 'rarities'];
+        if (!in_array($table, $allowedTables)) {throw new InvalidArgumentException("Table non autorisée");}
         $stmt = $pdo->query("SELECT * FROM $table");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
     public static function recordExists(PDO $pdo, string $table, int $id): bool{
+        $allowedTables = ['roles', 'rarities'];
+        if (!in_array($table, $allowedTables)) {throw new InvalidArgumentException("Table non autorisée");}
         $stmt = $pdo->prepare("SELECT 1 FROM $table WHERE id = ? LIMIT 1");
         $stmt->execute([$id]);
         return (bool) $stmt->fetchColumn();
     }
-
 
     public static function createCard(PDO $pdo, string $imgPath, int $rarityId, int $roleId, int $userId): void{
         $stmt = $pdo -> prepare("INSERT INTO cards (path, rarity_id, role_id, user_id) VALUES (?, ?, ?, ?)");
