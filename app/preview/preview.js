f8910b4e076ba404478fd7ac1b0e93d357004053
loictonneau75 <loictonneau75@gmail.com>
2026-02-23 09:39:56 +0100

    amélioration du responsive

--- app/preview/preview.js
+++ app/preview/preview.js
@@ -1,42 +1,46 @@
-const container = document.querySelector("#showCard");
-const preview = document.querySelector("#showCard > img");
+if (window.innerWidth >= 992) {
+    const container = document.querySelector("#showCard");
+    const preview = document.querySelector("#showCard > img");
 
-container.addEventListener("pointerover", (e) => {
-    if (e.target.classList.contains("card-img")) {
-        preview.src = e.target.src;
-        preview.style.display = "block";
-    }
-});
+    container.addEventListener("mouseover", (e) => {
+        if (e.target.classList.contains("card-img")) {
+            preview.src = e.target.src;
+            preview.style.display = "block";
+        }
+    });
 
-container.addEventListener("pointermove", (e) => {
-    if (!e.target.classList.contains("card-img")) return;
+    container.addEventListener("mousemove", (e) => {
+        if (!e.target.classList.contains("card-img")) return;
 
-    const offset = 20;
+        const offset = 20;
 
-    const previewWidth = preview.offsetWidth;
-    const previewHeight = preview.offsetHeight;
+        const previewWidth = preview.offsetWidth;
+        const previewHeight = preview.offsetHeight;
 
-    const viewportWidth = window.innerWidth;
-    const viewportHeight = window.innerHeight;
+        const viewportWidth = window.innerWidth;
+        const viewportHeight = window.innerHeight;
 
-    let left = e.pageX + offset;
-    let top = e.clientY + offset;
+        let left = e.pageX + offset;
+        let top = e.clientY + offset;
 
-    if (left + previewWidth > viewportWidth) {
-        left = e.pageX - previewWidth - offset;
-    }
+        if (left + previewWidth > viewportWidth) {
+            left = e.pageX - previewWidth - offset;
+        }
 
-    if (top + previewHeight > viewportHeight) {
-        top = e.clientY - previewHeight - offset;
-    }
+        if (top + previewHeight > viewportHeight) {
+            top = e.clientY - previewHeight - offset;
+        }
 
-    preview.style.left = left + "px";
-    preview.style.top = top + "px";
-});
+        preview.style.left = left + "px";
+        preview.style.top = top + "px";
+    });
 
 
-container.addEventListener("pointerout", (e) => {
-    if (e.target.classList.contains("card-img")) {
-        preview.style.display = "none";
-    }
-});
+    container.addEventListener("mouseout", (e) => {
+        if (e.target.classList.contains("card-img")) {
+            preview.style.display = "none";
+        }
+    });
+
+}
+
