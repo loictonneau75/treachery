57a251fb860675e3d674cad2141cf3ae36f764a0
Loic tonneau <loictonneau75@gmail.com>
2026-03-22 13:41:48 +0100

    oublie des ! corrigé

--- app/addCard/addCardForm.js
+++ app/addCard/addCardForm.js
@@ -4,9 +4,9 @@ import {resetSelect} from "../customSelect/customSelect.js"
 async function handleFormSubmitEvent(e, form){
     e.preventDefault();
     let errors = [];
-    if (form.querySelector("#cardRole").value.trim() === "") errors.push(["Veuillez choisir un role", [form.querySelector("#cardRoleInput > div")]]);
-    if (form.querySelector("#cardRarity").value.trim() === "") errors.push(["Veuillez choisir une rareté", [form.querySelector("#cardRarityInput > div")]]);
-    if (form.querySelector("#cardImg").files.length === 0) errors.push(["Veuillez choisir une image", [form.querySelector("#cardImgInput > label")]]);
+    if (form.querySelector("#cardRole").value.trim() === "") errors.push(["Veuillez choisir un role !", [form.querySelector("#cardRoleInput > div")]]);
+    if (form.querySelector("#cardRarity").value.trim() === "") errors.push(["Veuillez choisir une rareté !", [form.querySelector("#cardRarityInput > div")]]);
+    if (form.querySelector("#cardImg").files.length === 0) errors.push(["Veuillez choisir une image !", [form.querySelector("#cardImgInput > label")]]);
     if (errors.length >0) setErrors(errors, form);
     else await handlePostFormSubmit(errors, form);
 }
