746e6d04268e7b9edc93fd4bd1e335a694b37146
Loic tonneau <loictonneau75@gmail.com>
2026-03-23 09:15:35 +0100

    ajout du name a l'input

--- app/customInputNumber/customInputNumber.php
+++ app/customInputNumber/customInputNumber.php
@@ -8,7 +8,7 @@ class CustomInputNumber{
 ?>
         <div class="custom-input-number">
             <button type="button">+</button>
-            <input id="nbPlayers" type="number" min=<?=$min?> max=<?=$max?> placeholder="">
+            <input id="nbPlayers" name="nbPlayers" type="number" min=<?=$min?> max=<?=$max?> placeholder="">
             <button type="button">-</button>
         </div>
 <?php
