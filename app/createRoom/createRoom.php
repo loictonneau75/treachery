ba5bd765f1ddcf9061e2de6c19c71a7a9a96fba5
Loic tonneau <loictonneau75@gmail.com>
2026-05-18 18:32:59 +0200

    reorganisation plus logique

--- app/createRoom/createRoom.php
+++ app/createRoom/createRoom.php
@@ -39,11 +39,11 @@ function validateAmountOfCardsSelected(array $cardsSelected, int $nbPlayerinRoom
 
 function getRoleDistribution(int $nbPlayerinRoom): array {
     return match($nbPlayerinRoom) {
-        4 => [1 => 1, 4 => 1, 3 => 2, 2 => 0],
-        5 => [1 => 1, 4 => 1, 3 => 2, 2 => 1],
-        6 => [1 => 1, 4 => 1, 3 => 3, 2 => 1],
-        7 => [1 => 1, 4 => 1, 3 => 3, 2 => 2],
-        8 => [1 => 1, 4 => 2, 3 => 3, 2 => 2],
+        4 => [1 => 1, 2 => 0, 3 => 2, 4 => 1],
+        5 => [1 => 1, 2 => 1, 3 => 2, 4 => 1],
+        6 => [1 => 1, 2 => 1, 3 => 3, 4 => 1],
+        7 => [1 => 1, 2 => 2, 3 => 3, 4 => 1],
+        8 => [1 => 1, 2 => 2, 3 => 3, 4 => 2],
         default => []
     };
 }
